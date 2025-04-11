59cd031a9bc953e2dc0e613c1ba3131c26942fe6
khr818 <kharshanraghav@hotmail.com>
2025-04-11 16:17:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const userRoutes = require('./routes/users');
+const blogRoutes = require('./routes/blogs');
+
+const app = express();
+app.use(cors({ origin: true, credentials: true }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'secret-key', resave: false, saveUninitialized: true }));
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use('/api/users', userRoutes);
+app.use('/api/blogs', blogRoutes);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
